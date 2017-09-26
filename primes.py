beeb8c9a2090be99bf7bf38c82bfe43b30c9e1d9
kehernandez <kehernandez@chapman.edu>
2017-09-26 06:28:09 +0000

    made updates

--- primes.py
+++ primes.py
@@ -7,3 +7,11 @@ def eratosthenes(n):
     for i in range(2, math.ceil(math.sqrt(n))):
         all_num = list(filter(lambda x: (x%i != 0 or x == i) , all_num))
     return all_num
+
+def main():
+    args = sys.argv
+    n=int(args[1])
+    print(eratosthenes(n))
+    
+if __name__ == "__main__":
+    main(sys.argv)
